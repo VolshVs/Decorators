f49c1dd72385ea0e5b70e4c5cb2075dcdd7fd07e
VolshVs <89104047740@mail.ru>
2024-03-15 15:01:58 +0300

    Initial commit

---
+++ flat_generator.py
@@ -0,0 +1,33 @@
+import types
+
+from logger_2 import logger
+
+PATH = 'log_4.log'
+
+
+@logger(PATH)
+def flat_generator(list_of_lists):
+    new_list = []
+    for element in list_of_lists:
+        new_list.extend(element)
+    len_ = len(new_list)
+    for i in range(len_):
+        yield new_list[i]
+
+
+def test_3():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    print('Третий тест пройден!')
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
---
+++ main.py
@@ -0,0 +1,8 @@
+from flat_generator import test_3
+from logger_1 import test_1
+from logger_2 import test_2
+
+if __name__ == '__main__':
+    test_1()
+    test_2()
+    test_3()
